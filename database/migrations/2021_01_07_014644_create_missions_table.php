53b5286a444a941b7ef0e778b0f68069e6ac1b42
renissonsilva <renissonx@gmail.com>
2021-01-06 22:49:55 -0300

    Criando tabela das missões

---
+++ database/migrations/2021_01_07_014644_create_missions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('missions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('xp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('missions');
+    }
+}
