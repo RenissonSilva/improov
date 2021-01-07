e30bd9962ce5ad7ebadb7b744fe468184ebc4d3f
renissonsilva <renissonx@gmail.com>
2021-01-06 23:00:48 -0300

    Criando seed para missões

---
+++ database/seeds/MissionsTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Mission;
+
+class MissionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $items = [
+            [
+                "id" => 1,
+                "name" => "Commite em um projeto",
+                "xp" => "50",
+            ],
+            [
+                "id" => 2,
+                "name" => "Realize 4 commits",
+                "xp" => "50",
+            ],
+            [
+                "id" => 3,
+                "name" => "Realize 2 commits em um desafio",
+                "xp" => "50",
+            ],
+    ];
+
+    foreach ($items as $item) {
+        Mission::updateOrCreate(['id' => $item['id']], $item);
+    }
+    }
+}
