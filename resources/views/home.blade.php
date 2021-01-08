c5094898f2b2615394f7ef580ae52eecc79f534c
renissonsilva <renissonx@gmail.com>
2021-01-08 17:54:41 -0300

    Criando table onde mostra as missões do usuário

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,27 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <!-- <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    {{ __('You are logged in!') }}
-                </div>
-            </div> -->
-        </div>
+    <div class="row">
+        <h3 class="menu-title mr-4"><i class="fas fa-check-double icon-title"></i>Missões diárias</h3>
+        <h3 class="menu-title grey-text text-darken-1">1/2</h3>
     </div>
+    <!-- <div class="row">
+        <h3 class="col-9 menu-title"><i class="fas fa-check-double icon-title"></i>Missões diárias</h3>
+        <h3 class="col menu-title grey-text text-darken-1 right-align">1/2</h3>
+    </div> -->
+    <table class="striped">
+        <tbody>
+            @foreach($my_missions as $mission)
+            <tr class="row">
+                <td class="col-8 mission-text">{{$mission}}</td>
+                <td class="col valign-wrapper">
+                    <div class="progress">
+                        <div class="determinate" style="width: 70%;"></div>
+                    </div>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+      </table>
 </div>
 @endsection
