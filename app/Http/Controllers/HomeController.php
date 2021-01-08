ba8217fc7fe598106a5ca14c6f881805f667c611
renissonsilva <renissonx@gmail.com>
2021-01-08 13:51:27 -0300

    Gerando 2 missões novas caso tenha se passado 24h

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Auth;
 use App\User;
 use App\Mission;
+use App\Mission_user;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -35,6 +37,20 @@ class HomeController extends Controller
 
             foreach($taking_2_missions as $mission){
                 $user->mission()->attach($mission);
+            }
+        }else{
+            $getting_missions = Mission_user::where('user_id', $user->id)->latest()->limit(2)->get();
+
+            $data_inicial = Carbon::parse($getting_missions[0]->created_at->format('Y-m-d'));
+            $data_final = Carbon::parse(Carbon::now()->format('Y-m-d'));
+            $diferenca = $data_inicial->diff($data_final); 
+            $diferenca = $diferenca->format('%d');
+            if($diferenca >= 1){
+                $taking_2_missions = Mission::inRandomOrder()->limit(2)->get();
+
+                foreach($taking_2_missions as $mission){
+                    $user->mission()->attach($mission);
+                }
             }
         }
         return view('home');
---
+++ app/Mission_user.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mission_user extends Model
+{
+    protected $table = 'mission_user';
+}
