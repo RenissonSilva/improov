8f82467dc2a0d8a2d61abacfda2cfe4786530784
renissonsilva <renissonx@gmail.com>
2021-01-08 11:16:12 -0300

    Separando blade layout da home e logout funcional

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Improov</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -15,67 +15,66 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Saira&display=swap" rel="stylesheet"> 
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/layout-app.css') }}" rel="stylesheet">
-
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </head>
 <body>
     <div id="app">
-        <div class="main">
-            <div class="loggedNavbar"></div>
-            <nav class="navbar navbar-expand-md navbar-transparent">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{ url('images/logo.png') }}" class="logo">
+                    {{ config('app.name', 'Laravel') }}
                 </a>
-                <h4>Improov</h4>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto" >
-                        <li class="nav-item">
-                            <a class="dropdown-item item-navbar" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                    {{ __('Sair') }}
-                            </a>
-                        </li>
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
                     </ul>
                 </div>
-            </nav>
-            <div class="avatar-div">
-                <img src="{{ url('images/avatar.png') }}" class="avatar">
             </div>
-            
-        </div>
+        </nav>
+
         <main class="py-4">
             @yield('content')
         </main>
-        <footer class="page-footer valign-wrapper">
-            <div class="container">
-                <div class="row">
-                    <div class="col-3 l2 s2 valign-wrapper">
-                        <img src="{{ url('images/logo.png') }}" class="logo-footer">
-                        <p class="white-text text-footer">Improov</p>
-                    </div>
-                    <div class="col l10 s6 hide-on-med-and-down link-footer">
-                        <a class="grey-text text-lighten-3 right" href="#!">Ajuda</a>
-                        <a class="grey-text text-lighten-3 right" href="#!">Termos de privacidade</a>
-                        <a class="grey-text text-lighten-3 right" href="#!">Fale conosco</a>
-                    </div>
-                </div>
-            </div>
-        </footer>
     </div>
-    
 </body>
 </html>
---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Improov</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Saira&display=swap" rel="stylesheet"> 
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/layout-app.css') }}" rel="stylesheet">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</head>
+<body>
+    <div id="app">
+        <div class="main">
+            <div class="loggedNavbar"></div>
+            <nav class="navbar navbar-expand-md navbar-transparent">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="{{ url('images/logo.png') }}" class="logo">
+                </a>
+                <h4>Improov</h4>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto" >
+                        <li class="nav-item">
+                            <a class="dropdown-item item-navbar" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                    {{ __('Sair') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="avatar-div">
+                <img src="{{ url('images/avatar.png') }}" class="avatar">
+            </div>
+            
+        </div>
+        <main class="py-4">
+            @yield('content')
+        </main>
+        <footer class="page-footer valign-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-3 l2 s2 valign-wrapper">
+                        <img src="{{ url('images/logo.png') }}" class="logo-footer">
+                        <p class="white-text text-footer">Improov</p>
+                    </div>
+                    <div class="col l10 s6 hide-on-med-and-down link-footer">
+                        <a class="grey-text text-lighten-3 right" href="#!">Ajuda</a>
+                        <a class="grey-text text-lighten-3 right" href="#!">Termos de privacidade</a>
+                        <a class="grey-text text-lighten-3 right" href="#!">Fale conosco</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    
+</body>
+</html>
