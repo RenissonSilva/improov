b8bac9b9ed71fcd5812c83e13408446e5a647aca
renissonsilva <renissonx@gmail.com>
2021-01-08 12:26:10 -0300

    Pegando 2 missões aleatórias e linkando com o usuário se ele não tiver nenhuma

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\User;
+use App\Mission;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,17 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $user = User::where('id', Auth::id())->first();
+        $missions = Mission::all();
+        $mission_user = $user->mission()->get();
+        
+        if($mission_user->isEmpty()) {
+            $taking_2_missions = Mission::inRandomOrder()->limit(2)->get();
+
+            foreach($taking_2_missions as $mission){
+                $user->mission()->attach($mission);
+            }
+        }
         return view('home');
     }
 }
--- app/User.php
+++ app/User.php
@@ -30,7 +30,7 @@ class User extends Authenticatable
 
     public function mission()
     {
-        return $this->belongsToMany(Mission::class);
+        return $this->belongsToMany(Mission::class)->withTimestamps();
     }
 
     /**
