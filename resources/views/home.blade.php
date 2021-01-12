21caab77f5d06b3bf43c344fba9c4eb6d075dbe4
renissonsilva <renissonx@gmail.com>
2021-01-12 20:56:24 -0300

    Alterando filtro para um tom azulado e alterando alguns dados estáticos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <h3 class="col-9 menu-title"><i class="fas fa-check-double icon-title"></i>Missões diárias</h3>
-        <h3 class="col menu-title grey-text text-darken-1 right-align">1/2</h3>
+        <h3 class="col menu-title grey-text text-darken-1 right-align">0/2</h3>
     </div>
     <table class="striped">
     <tbody>
@@ -13,7 +13,7 @@
             <td class="col-8 mission-text">{{$mission}}</td>
             <td class="col valign-wrapper">
                 <div class="progress">
-                    <div class="determinate" style="width: 70%;"></div>
+                    <div class="determinate" style="width: 50%;"></div>
                 </div>
             </td>
         </tr>
