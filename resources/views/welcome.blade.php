d98daa3aaf7dee303c6f031ed311628206143435
renissonsilva <renissonx@gmail.com>
2021-01-14 17:04:16 -0300

    Criando parte de vantagens na welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,6 +34,45 @@
             <span class="descricao">Viemos para te ajudar a focar em seus objetivos e a aperfeiçoar seus conhecimentos para que você esteja mais perto da força!</span>
         </div>
     </div>
+
+    <div class="container-default row container-advantages">
+        <div class="col s3 m4 l3">
+            <img src="{{ url('images/mission.png') }}" class="icon-about">
+        </div>
+        <div class="col s9 m8 l9">
+            <p class="title-advantage">Missões diárias</p>
+            <p class="text-advantage">Tenha uma série de objetivos e desafios a serem alcançados todos os dias.</p>
+        </div>
+    </div>
+
+    <div class="container-default"><div class="line-advantage"></div></div>
+
+    <div class="container-default row container-advantages">
+        <div class="col s9 m8 l9">
+            <p class="title-advantage">Ranking</p>
+            <p class="text-advantage">Compare a sua pontuação com as de qualquer usuário do sistema.</p>
+        </div>
+        <div class="col s3 m4 l3">
+            <img src="{{ url('images/ranking.png') }}" class="icon-about right">
+        </div>
+    </div>
+
+    <div class="container-default"><div class="line-advantage"></div></div>
+
+    <div class="container-default row container-advantages">
+        <div class="col s1 m4 l4 center-align">
+            <p class="text-advantage last-3-advantages">Alcance seus objetivos</p>
+            <img src="{{ url('images/goals.png') }}" class="icon-about">
+        </div>
+        <div class="col s1 m4 l4 center-align">
+            <p class="text-advantage last-3-advantages">Aperfeiçoe seus conhecimentos</p>
+            <img src="{{ url('images/knowledge.png') }}" class="icon-about">
+        </div>
+        <div class="col s1 m4 l4 center-align">
+            <p class="text-advantage last-3-advantages">Promoção em cursos</p>
+            <img src="{{ url('images/discount.png') }}" class="icon-about">
+        </div>
+    </div>
     
   <!--  Scripts-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
