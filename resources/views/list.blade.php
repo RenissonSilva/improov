24d34d7b02c1eece0d25edf6c6449ff981f1cabf
Renisson Silva <renisson@cea.eti.br>
2021-01-28 01:10:28 -0300

    Adicionando imagem de acordo com a linguagem e aumentando tamanho

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -9,11 +9,12 @@
 
         <div class="row">
             @foreach($github_repo as $repo)
+            @php ($repo->language) ? $url_image = "images/languages/$repo->language.png" : $url_image = "images/languages/default.png"; @endphp
             <div class="col-md-6">
                 <div class="card-panel grey lighten-5 z-depth-1 card-margin-default card-margin-{{$repo->language}} card-repo">
                     <div class="row nm-row">
                         <div>
-                            <img src="{{ url('images/languages/js.png') }}" class="circle responsive-img repo-language">
+                            <img src="{{ url($url_image) }}" class="circle responsive-img repo-language">
                         </div>
                         <div class="col-md-9">
                             <span class="repo-title">
