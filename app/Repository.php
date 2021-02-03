0aed3d0c209d4438d17826e5409f2746a3312ddf
RenissonSilva <renisson@cea.eti.br>
2021-02-02 23:23:14 -0300

    Criando migration e model para guardar repositorios

---
+++ app/Repository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Repository extends Model
+{
+    protected $fillable = [
+        'name', 'main_language', 'link',
+    ];
+}
