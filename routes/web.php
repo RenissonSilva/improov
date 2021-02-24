6a35a69ff204f46b467de94e37a65ac34de4adb4
RenissonSilva <renissonx@gmail.com>
2021-02-24 10:01:31 -0300

    Bloqueando tela inicial se estiver logado

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('welcome');
+})->middleware('guest')->name('welcome');
 
 Auth::routes();
 
