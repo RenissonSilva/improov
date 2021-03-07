bd032bf953c541c5e7d536eb6f15ec6b9ba064bc
RenissonSilva <renissonx@gmail.com>
2021-03-07 17:35:01 -0300

    Melhora no alinhamento dos cards de projetos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,6 @@
 <div class="container-default">
     <div class="row">
         <h3 class="col-9 menu-title"><i class="fas fa-check icon-title"></i>Missões diárias</h3>
-        <h3 class="col menu-title grey-text text-darken-1 right-align">{{ $completed_missions }}/{{ $total_missions }}</h3>
     </div>
     <table class="striped">
         <tbody>
@@ -41,7 +40,7 @@
     <div class="row justify-content-around mt-5" style="height:900px;margin-bottom:90px">
         <div class="col-6 np">
             <div class="tituloProjetoFavorito">
-                <h3 class="col-12 menu-title"><i class="fas fa-folder icon-title"></i>Projetos Favoritos</h3>
+                <h3 class="col-12 menu-title nmt"><i class="fas fa-folder icon-title"></i>Projetos Favoritos</h3>
             </div>
             @if($favorites_repositories->isEmpty())
             <div class="row">
@@ -60,16 +59,16 @@
             <div class="col-md-12">
                 <div class="card-panel grey lighten-5 z-depth-1 card-margin-default card-margin-{{$fav_repo->main_language}} card-repo h-160">
                     <div class="row nm-row h-100">
-                        <div>
-                            <img src="{{ url($url_image) }}" class="circle responsive-img repo-language">
+                        <div class="col-md-3">
+                            <img src="{{ url($url_image) }}" class="circle responsive-img repo-language" style="min-width:80px;">
                         </div>
-                        <div class="col-md-8 name-star">
-                            <div>
-                                <span class="repo-title">
+                        <div class="col-md-8 name-star np">
+                            <div class="truncate-custom">
+                                <span class="repo-title tooltipped" data-position="bottom" data-html="true" data-tooltip="{{ $fav_repo->name }}">
                                     {{ $fav_repo->name }}
                                 </span>
                             </div>
-                            <div class="right-align div-btn-github">
+                            <div class="right-align div-btn-github col np">
                                 <a class="waves-effect waves-light btn modal-trigger btn-default btn-github" href="{{ $fav_repo->link }}" target="_blank">Ver no github</a>
                             </div>
                         </div>
