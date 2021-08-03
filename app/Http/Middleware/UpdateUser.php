c87e1124268ba4391d84a0b8c29e8e67269feee0
Erickson Ferreira <erickson.nascimento@grupohorizonte.com.br>
2021-08-03 09:14:40 -0300

    modificacoes no middleware

--- app/Http/Middleware/UpdateUser.php
+++ app/Http/Middleware/UpdateUser.php
@@ -20,40 +20,40 @@ class UpdateUser
     public function handle($request, Closure $next)
     {
         $user = Auth::user();
-        $focus = 0;
-        $last_update = Carbon::createFromDate($user->github_last_update);
-        if($user->github_last_update === null || !$last_update->isToday()) {
-            $commitsLastMonth = RequisicaoController::getCommitsLastMonth($user->name);
+        // $focus = 0;
+        // $last_update = Carbon::createFromDate($user->github_last_update);
+        // if($user->github_last_update === null || !$last_update->isToday()) {
+        //     $commitsLastMonth = RequisicaoController::getCommitsLastMonth($user->name);
 
-            // foreach($commitsLastMonth as $commit) {
-            //     $created_at = Carbon::create($commit['created_at'])->subHours(3);
+        //     // foreach($commitsLastMonth as $commit) {
+        //     //     $created_at = Carbon::create($commit['created_at'])->subHours(3);
 
-            //     Commit::updateOrCreate([
-            //         'user_id' => $user->id,
-            //         'created_at' => $created_at
-            //     ]);
-            // }
+        //     //     Commit::updateOrCreate([
+        //     //         'user_id' => $user->id,
+        //     //         'created_at' => $created_at
+        //     //     ]);
+        //     // }
 
-            $day = Carbon::now()->format('Y-m-d');
-            $commitsFromUser = $user->commits()->orderBy('created_at', 'DESC')->get();
+        //     $day = Carbon::now()->format('Y-m-d');
+        //     $commitsFromUser = $user->commits()->orderBy('created_at', 'DESC')->get();
 
-            foreach($commitsFromUser as $c) {
-                $cday = new Carbon($c->created_at);
-                $formatted_cday = $cday->format('Y-m-d');
-                $diff = Carbon::parse($day)->diffInDays($cday->format('Y-m-d'));
+        //     foreach($commitsFromUser as $c) {
+        //         $cday = new Carbon($c->created_at);
+        //         $formatted_cday = $cday->format('Y-m-d');
+        //         $diff = Carbon::parse($day)->diffInDays($cday->format('Y-m-d'));
 
-                if ($diff === 1 || $day === $formatted_cday) {
-                    $day = $cday;
-                    $focus++;
-                }
-            }
-            $user->focus_days = $focus;
-            if($focus > $user->max_days_in_focus){
-                $user->max_days_in_focus = $focus;
-            }
-            $user->github_last_update = Carbon::now();
-            $user->save();
-        }
+        //         if ($diff === 1 || $day === $formatted_cday) {
+        //             $day = $cday;
+        //             $focus++;
+        //         }
+        //     }
+        //     $user->focus_days = $focus;
+        //     if($focus > $user->max_days_in_focus){
+        //         $user->max_days_in_focus = $focus;
+        //     }
+        //     $user->github_last_update = Carbon::now();
+        //     $user->save();
+        // }
 
         return $next($request);
     }
